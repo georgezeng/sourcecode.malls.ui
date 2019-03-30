b95f2b15b9b53a442736249eda9a8852a7a6da96
georgezeng <zzeng@bglcorp.com.au>
2019-03-30 16:33:37 +0800

    no message

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -106,7 +106,7 @@ export default {
       })
     },
     // 退出登录
-    handleLogOut ({ state, commit }) {
+    handleLogOut ({ state, commit, rootState }) {
       let res = rootState.app.tagNavList.filter(item => item.name === config.homeName)
       commit('setTagNavList', res)
       return new Promise((resolve, reject) => {
